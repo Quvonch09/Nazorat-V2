5a592d4097aff82f48399b97bbed2426df58a018
Muhammad <muhammadnazirqulov1@gmail.com>
2025-11-29 13:50:44 +0500

    1 chi

--- src/main/java/com/example/nazoratv2/entity/Attendance.java
+++ src/main/java/com/example/nazoratv2/entity/Attendance.java
@@ -2,6 +2,7 @@ package com.example.nazoratv2.entity;
 
 import com.example.nazoratv2.entity.base.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ public class Attendance extends BaseEntity {
 
     private LocalDate date;
 
+    @ManyToOne
     private Student student;
 
     private Boolean status; //true -> keldi, false -> kelmadi, null -> sababli
