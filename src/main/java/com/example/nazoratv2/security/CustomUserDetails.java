5ca7e26131f0ae09b9762c0e059aa5d72759d9ba
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2026-01-14 16:27:57 +0500

    BISMILLAH 18 (exception tugrilandi)

--- src/main/java/com/example/nazoratv2/security/CustomUserDetails.java
+++ src/main/java/com/example/nazoratv2/security/CustomUserDetails.java
@@ -20,7 +20,7 @@ public class CustomUserDetails implements UserDetails {
     private final String password;
     private final String role;
     private final String imgUrl;
-    private final boolean enabled;
+    private final boolean active;
 
     public CustomUserDetails(User user) {
         this.user = user;
@@ -30,7 +30,7 @@ public class CustomUserDetails implements UserDetails {
         this.password = user.getPassword();
         this.role = user.getRole().name();
         this.imgUrl = user.getImageUrl();
-        this.enabled = user.isActive();
+        this.active = user.isActive();
     }
 
     public CustomUserDetails(Student student) {
@@ -41,7 +41,7 @@ public class CustomUserDetails implements UserDetails {
         this.password = student.getPassword();
         this.role = "ROLE_STUDENT";
         this.imgUrl = student.getImgUrl();
-        this.enabled = student.isActive();
+        this.active = student.isActive();
     }
 
     public boolean isUser() {
@@ -62,7 +62,7 @@ public class CustomUserDetails implements UserDetails {
     @Override public boolean isAccountNonExpired() { return true; }
     @Override public boolean isAccountNonLocked() { return true; }
     @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return enabled; }
+    @Override public boolean isEnabled() { return active; }
 
     public static CustomUserDetails fromUser(User user) {
         return new CustomUserDetails(user);
