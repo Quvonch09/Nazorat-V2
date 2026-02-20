93a88450da0a26a8b56795947f3f4328aa8eb230
Muhammad <muhammadnazirqulov1@gmail.com>
2026-02-20 17:48:43 +0500

    1 chi

--- src/main/java/com/example/nazoratv2/controller/DashboardController.java
+++ src/main/java/com/example/nazoratv2/controller/DashboardController.java
@@ -34,14 +34,13 @@ public class DashboardController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_SUPER_ADMIN')")
-
     public ResponseEntity<ApiResponse<DashboardDTO>> getDashboard() {
         return ResponseEntity.ok(dashboardService.getDashboard());
     }
 
     @GetMapping("/top-students")
-    public ApiResponse<List<TopStudentDto>> topStudents(@AuthenticationPrincipal UserDetails user) {
-        return studentService.getTop5Students(user);
+    public ResponseEntity<ApiResponse<List<TopStudentDto>>> topStudents(@AuthenticationPrincipal UserDetails user) {
+        return ResponseEntity.ok(studentService.getTop5Students(user));
     }
 
     @GetMapping("/schedule")
